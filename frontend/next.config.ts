10e17d56bb3fbbd4389fa309147e5abc7ba037b7
Elielthon (Leto) <letocrazy@hotmail.com>
2025-04-03 13:24:32 -0300

    chore(frontend): configurando deploy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,15 @@
-import type { NextConfig } from "next"
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+	output: 'export',
 	images: {
 		remotePatterns: [
 			{
-				protocol: "https",
-				hostname: "**",
+				protocol: 'https',
+				hostname: '**',
 			},
 		],
 	},
-}
+};
 
-export default nextConfig
+export default nextConfig;
